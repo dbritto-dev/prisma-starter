b4367c1d20db9631c2f0e089bc72d183cc40b715
danilobrinu <ddbn.c2@gmail.com>
2018-04-29 13:16:39 -0500

    init project

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
+const resolvers = require('./resolvers')
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  resolverValidationOptions: {
+    requireResolversForResolveType: false
+  },
+  context: (req) => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: './src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
+      endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma API
+      debug: true, // log all GraphQL queries & mutations sent to the Prisma API
+      // secret: process.env.PRISMA_SECRET, // only needed if specified in `database/prisma.yml`
+    }),
+  }),
+})
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
